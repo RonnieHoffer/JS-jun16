406f2cbf2a057ef57da5fad2897babe3b7466efd
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-20 21:22:29 -0400

    Another file and folder rearrangement

---
+++ assign1/pacman-console-log.js
@@ -0,0 +1,12 @@
+console.log('Pac-Man eats ghosts for lunch.');
+
+// const ghost = 'Blinky'; // The rest of this line is comment.
+// console.log(ghost);
+
+// var x;
+// console.log(x);
+// let y;
+// console.log(y);
+
+let ghost = 'Inky';
+console.log(ghost);
