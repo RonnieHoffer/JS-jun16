32a29b187379f1b8d9eb0d48368208d44a01d928
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-23 23:58:36 -0400

    JavaScript Assignment 1 completed

---
+++ assign1/pacman-functions.js
@@ -0,0 +1,16 @@
+// // Named function
+// function eatDot() {
+//     console.log("MUNCH!");
+// }
+
+// // Anonymous function
+// var eatDot = function() {
+//     console.log("MUNCH!");
+// }
+
+//Arrow function (also anonymous)
+var eatDot = () => {
+    console.log("MUNCH!");
+}
+
+eatDot();
