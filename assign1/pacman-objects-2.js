bec77acb12e922baa24dfaf353b9d9bbcf1d3666
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-23 22:30:49 -0400

    Up to and including pacman-objects-2.js

---
+++ assign1/pacman-objects-2.js
@@ -0,0 +1,14 @@
+const ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
+
+console.log(ghosts.clyde);
+console.log(ghosts['clyde']);
+const ghostName = 'clyde'
+console.log(ghosts[ghostName]);
+console.log(ghosts.ghostName);
